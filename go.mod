96afbfed7f17a33d43bedad09ef8d4e20072ddbb
zlobste <nick.krainiuk@resolute-datascience.com>
2021-09-18 23:32:21 +0300

    Define binance http client

--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/valyala/fasthttp v1.30.0
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 )
--- internal/services/buyer/main.go
+++ internal/services/buyer/main.go
@@ -2,8 +2,11 @@ package buyer
 
 import (
 	"context"
+	"fmt"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/zlobste/binance-mystery-buyer/internal/config"
+	"github.com/zlobste/binance-mystery-buyer/pkg/binance"
 )
 
 type Service interface {
@@ -11,12 +14,15 @@ type Service interface {
 }
 
 type service struct {
+	client binance.Client
 	config config.Config
 	logger *logrus.Logger
 }
 
 func New(cfg config.Config) Service {
+	auth := cfg.GetAuth()
 	return &service{
+		client: binance.New(auth.Proxy).WithSigner(auth.CSRFToken, auth.Cookie),
 		config: cfg,
 		logger: cfg.Log(),
 	}
@@ -24,6 +30,13 @@ func New(cfg config.Config) Service {
 
 func (s *service) Run(ctx context.Context) error {
 	s.logger.Info("Buyer has started...")
+
+	info, err := s.client.GetSignerInfo()
+	if err != nil {
+		return errors.Wrap(err, "error on getting user info")
+	}
+
+	fmt.Println(fmt.Sprintf("User info: %s", info))
 
 	return nil
 }
